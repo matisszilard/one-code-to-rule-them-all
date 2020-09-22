ec7f8a50153de84ee379561f983d887728756a14
Szilárd Mátis <matis.szilard@codespring.ro>
2020-09-22 13:15:28 +0300

    Updated Jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,25 +1,20 @@
 
 def buildLinux(buildType = "Release") {
-    sh 'echo BUILD_TYPE [${buildType}]'
     sh 'make clean && mkdir -p build'
-    sh 'cd build && cmake .. -DCMAKE_BUILD_TYPE=${buildType} && cmake --build . --config ${buildType}'
+    sh 'cd build && cmake .. -DCMAKE_BUILD_TYPE='+ buildType + ' && cmake --build . --config '+ buildType
     sh 'mv ./build/librule.a librule-linux-amd64.a'
 }
 
 def buildMacOS(buildType = "Release") {
-    sh 'echo BUILD_TYPE [${BUILD_TYPE}]'
     sh 'make clean && mkdir -p build'
-    sh 'cd build && cmake .. -DCMAKE_BUILD_TYPE=${buildType} && cmake --build . --config ${buildType}'
+    sh 'cd build && cmake .. -DCMAKE_BUILD_TYPE='+ buildType + ' && cmake --build . --config '+ buildType
     sh 'mv ./build/librule.a librule-darwin-x86_64.a'
 }
 
 def buildiOS(platform, buildType = "Release") {
-    sh 'echo BUILD_TYPE [${buildType}]'
-    sh 'echo BUILD_TYPE ['+ buildType + ']'
-    println(buildType);
     sh 'make clean && mkdir -p build'
-    sh 'cd build && cmake .. -DCMAKE_BUILD_TYPE=${buildType} -DCMAKE_TOOLCHAIN_FILE=../toolchains/iOS.toolchain.cmake -GXcode -DPLATFORM=${platform} && cmake --build . --config ${buildType}'
-    sh 'mv ./build/librule.a librule-ios-${platform}.a'
+    sh 'cd build && cmake .. -DCMAKE_BUILD_TYPE='+ buildType + ' -DCMAKE_TOOLCHAIN_FILE=../toolchains/iOS.toolchain.cmake -GXcode -DPLATFORM=' + platform + ' && cmake --build . --config ' + buildType
+    sh 'mv ./build/librule.a librule-ios-' + platform + '.a'
 }
 
 return this
