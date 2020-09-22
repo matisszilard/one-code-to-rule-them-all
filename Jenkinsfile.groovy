1516c5e9176c020f958cf3f0b896f7ab7cb46597
Szilárd Mátis <matis.szilard@codespring.ro>
2020-09-22 12:35:43 +0300

    Updated Jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,17 +1,17 @@
 
-buildLinux() {
+def buildLinux() {
     sh 'make clean && mkdir -p build'
     sh 'cd build && cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} && cmake --build . --config ${BUILD_TYPE}'
     sh 'mv ./build/librule.a librule-linux-amd64.a'
 }
 
-buildMacOS() {
+def buildMacOS() {
     sh 'make clean && mkdir -p build'
     sh 'cd build && cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} && cmake --build . --config ${BUILD_TYPE}'
     sh 'mv ./build/librule.a librule-darwin-x86_64.a'
 }
 
-buildiOS() {
+def buildiOS() {
     sh 'make clean && mkdir -p build'
     sh 'cd build && cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_TOOLCHAIN_FILE=../toolchains/iOS.toolchain.cmake -GXcode -DPLATFORM=${PLATFORM} && cmake --build . --config ${BUILD_TYPE}'
     sh 'mv ./build/librule.a librule-ios-${PLATFORM}.a'
